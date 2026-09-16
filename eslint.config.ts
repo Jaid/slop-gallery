1550d1cfe5ec15340298b20938c984369efcf6bc
Jaid <6216144+Jaid@users.noreply.github.com>
2026-09-16 18:53:49 +0100

    Removed manual exclusion again
    Signed-off-by: Jaid <6216144+Jaid@users.noreply.github.com>

--- eslint.config.ts
+++ eslint.config.ts
@@ -4,7 +4,7 @@ import {makeEslintConfig} from 'eslint-config-jaid'
 
 const config: Array<Linter.Config> = [
   {ignores: ['private/**', 'out/**', 'dist/**', 'temp/**']},
-  ...makeEslintConfig({excludeRules: ['stylistic/type-generic-spacing']}),
+  ...makeEslintConfig(),
 ]
 
 export default config
