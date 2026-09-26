51593db7ee21e6cc1f00df54dd3d671fc28049a2
Jaid <6216144+Jaid@users.noreply.github.com>
2026-09-26 07:39:49 +0100

    Added `--keep_invisible 0` to the JXL encoding options
    Signed-off-by: Jaid <6216144+Jaid@users.noreply.github.com>

--- packages/knot-materials/scripts/lib/encodeJxl.ts
+++ packages/knot-materials/scripts/lib/encodeJxl.ts
@@ -1,4 +1,4 @@
-export const lossyJxlOptions = (distance = 1) => ['--effort', '10', '--brotli_effort', '11', '--distance', String(distance)]
+export const lossyJxlOptions = (distance = 1) => ['--effort', '10', '--brotli_effort', '11', '--distance', String(distance), '--keep_invisible', '0'] as const
 
 export async function encodeJxl(input: string, output: string) {
   await Bun.$`cjxl ${input} ${lossyJxlOptions()} ${output}`.quiet()
